b4e138a73c496c6d9ece5e47e0ef78d679e0e975
Ray Ventura <circfox@yahoo.com>
2018-11-13 21:03:57 -0800

    added files

---
+++ client/src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => (
+<div className="card text-white bg-info mb-3" style={{ "max-width": "18rem;"}}>
+
+<div className="card-body">
+    <h2 className="card-title">(React) Google Books Search </h2>
+    <p className="card-text">Search for and Save Books for Interest.</p>
+  </div>
+  </div>
+  );
+
+  export default Title;
